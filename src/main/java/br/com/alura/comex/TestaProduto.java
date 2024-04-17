7156e95a9b3cc06970e072b3a15e50679a8335a9
Leonardo Silva <leonardo.inacio@ifood.com.br>
2024-04-17 19:35:55 -0300

    Criação da semana 6, primeira mentoria

---
+++ src/main/java/br/com/alura/comex/TestaProduto.java
@@ -0,0 +1,28 @@
+package br.com.alura.comex;
+
+public class TestaProduto {
+
+    public static void main(String[] args) {
+
+        Produto celular = new Produto();
+        celular.setNome("Samsung Galaxy S50");
+        celular.setQuantidade(10);
+        celular.setDescricao("Celular Android com 512 GB");
+        celular.setPrecoUnitario(1350.59);
+
+        Produto celular2 = new Produto();
+        celular2.setNome("Samsung Galaxy S50");
+        celular2.setQuantidade(5);
+        celular2.setDescricao("Celular Android com 512 GB");
+        celular2.setPrecoUnitario(3500);
+
+        System.out.println(celular);
+        System.out.println(celular2);
+
+        if (celular.equals(celular2)){
+            System.out.println("São iguais!");
+        } else {
+            System.out.println("Não são!");
+        }
+    }
+}
